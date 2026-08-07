9b632c22fd1639c1751d8b83a34ee6bc029eafab
Sunarso29 <sunarso29@gmail.com>
2026-08-07 23:31:37 +0700

    V77 refresh cache for restored colorful Islamic UI

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="paibp-smart-v76-static";
+const CACHE_NAME="paibp-smart-v77-colorful-static";
 const STATIC=["./logo-spensus.png","./assets/icons/icon-192.png"];
 
 self.addEventListener("install",(event)=>{
@@ -21,7 +21,7 @@ self.addEventListener("fetch",(event)=>{
 
   const fresh = event.request.mode==="navigate"
     || ["script","style","document"].includes(event.request.destination)
-    || /app-config|islamic-lite|islamic-data|islamic-learning|islamic-upgrade|khutbah|hadith|arabic|script\.js|teacher-cat|cat-session|net-v71|mobile-fix|stable-v72|service-worker/i.test(url.pathname);
+    || /app-config|v38-upgrade|v39-upgrade|islamic-lite|islamic-data|islamic-learning|islamic-upgrade|khutbah|hadith|arabic|script\.js|teacher-cat|cat-session|net-v71|mobile-fix|stable-v72|service-worker/i.test(url.pathname);
 
   if(fresh){
     event.respondWith(
