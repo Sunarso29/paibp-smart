a022997e2a97fb1e345344acde4d033c1401909f
Sunarso29 <sunarso29@gmail.com>
2026-08-13 23:23:34 +0700

    Hotfix legacy service worker hentikan recovery dan redirect langsung

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,3 @@
-const RECOVERY_URL = new URL('./index.html?recovery=v130', self.location.href).href;
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    try {
-      const keys = await caches.keys();
-      await Promise.all(keys.filter(k => /paibp|smart|spensus/i.test(k)).map(k => caches.delete(k)));
-    } catch (_) {}
-    try { await self.registration.unregister(); } catch (_) {}
-    try {
-      const windows = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
-      await Promise.all(windows.map(client => client.navigate(RECOVERY_URL).catch(() => null)));
-    } catch (_) {}
-  })());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => new Response('PAIBP SMART sedang memulihkan alamat lama.', {
-    status: 503,
-    headers: { 'Content-Type': 'text/plain; charset=utf-8', 'Cache-Control': 'no-store' }
-  })));
-});
+self.addEventListener('install',event=>{self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil((async()=>{try{for(const k of await caches.keys())await caches.delete(k)}catch(_){}try{await self.registration.unregister()}catch(_){}try{for(const c of await self.clients.matchAll({type:'window',includeUncontrolled:true})){const u=new URL(c.url);let p=u.pathname.replace(/^\/paibp-smart/,'')||'/';c.navigate('https://paibpsmart.github.io'+p+u.search+u.hash).catch(()=>null)}}catch(_){}})());});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}));});
